d56a05c64ed8acaf05b9b0ce88d33f36153fac56
Rigoberto Saenz <beto456789@gmail.com>
2019-10-08 16:13:32 -0500

    Empty Xcode project added

---
+++ QA Interview Mobile App/QA Interview Mobile App/View/ViewController/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  QA Interview Mobile App
+//
+//  Created by Rigoberto Saenz Imbacuan on 10/8/19.
+//  Copyright © 2019 Belatrix Software. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
